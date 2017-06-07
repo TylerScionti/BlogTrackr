d09328218c9b4f8aab35b7952d90cf4d6b80b14a
Tyler Scionti <tscionti@blogtrackr.com>
2017-06-06 22:01:27 -0400

    Added entire site crawler branch
    Added an entire site crawler capability, looks to be correctly catching all the links, need to ensure it saves and follows them correctly!

--- spider.py
+++ spider.py
@@ -1,16 +1,24 @@
 import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import HtmlXPathSelector
 from blogtrackr_sample.items import BlogTrackrSampleItem
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "blogtrackr"
-    start_urls = [
-        'http://blogtrackr.com/blog',
-    ]
+class QuotesSpider(CrawlSpider):
 
-    def parse(self, response):
-        for title in response.css('h1'):
-        	yield {
-        	'text': title.css('h1').extract()
-        	}
+	name = "blogtrackr"
+	allowed_domains = ["blogtrackr.com"]
+	start_urls = [
+	'http://blogtrackr.com/blog']
+	rules = [Rule(LinkExtractor(allow=[r'.*']), follow=True),
+            Rule(LinkExtractor(allow=[r'@href']), callback='parse_links')]
+	
+	def parse_links(self, response):
+		filename = "test.txt"
+		archive = open(filename, 'wb')
+		extractor = LinkExtractor(allow=r'blog\d+')
+		for link in extractor.extract_links(response):
+			url = link.urlextractor = LinkExtractor(allow=r'blog\d+')
+			achrive.writelines("%s\n" % url)                
+			print(url)
