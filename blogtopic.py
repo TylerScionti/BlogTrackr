ecefe380e4b23df27970b0ba43953ca82f1e97cd
Tyler Scionti <tscionti@blogtrackr.com>
2017-06-08 21:30:51 -0400

    Added removal of stop words
    Added removal of stop words from raw text input using the nltk corpus

--- blogtopic.py
+++ blogtopic.py
@@ -1,4 +1,5 @@
 import nltk
+from nltk.corpus import stopwords
 from nltk import FreqDist, word_tokenize
 from nltk.collocations import *
 import feedparser
@@ -7,43 +8,44 @@ from bs4 import BeautifulSoup
 
 bigram_measures = nltk.collocations.BigramAssocMeasures()
 
-print("Hello! Enter your blog's RSS feed and we'll tell you what it's about!")
-url = input('Enter RSS feed here: ')
+url = input("Enter a RSS feed url: ")
 
-#parses the blog feed URL
-blog = feedparser.parse(url)
-blog['feed']['title']
-#selects the first blog post from the feed
-post = blog.entries[0]
-content = post.content[0].value
-#strips the feed of extra markup, retains the raw text
-soup = BeautifulSoup(content, "html.parser")
-content = soup.findAll('p')
-stuff = str(content)
+feed = feedparser.parse(url)
+
+for entry in feed['entries']:
+	content = urlopen(entry['link']).read()
+	soup = BeautifulSoup(content, "html.parser")
+	content = soup.findAll('p')
+	stuff = str(content)
 
 text = []
-#breaks the text into individual words in a list 'text'
+
 for word in stuff.split():
 	if word.isalpha():
 		text.append(word)
 
-#finds words longer than 5 letters and used more than 2 times
-fdist1 = FreqDist(text)
-words = sorted(w for w in set(text) if len(w) > 5 and fdist1[w] > 2)
-#prints the words to the screen
+stop_words = set(stopwords.words('english'))
+clean_tokens = [w for w in text if not w in stop_words]
+
+
+fdist1 = FreqDist(clean_tokens)
+words = sorted(w for w in set(clean_tokens) if len(w) > 2 and fdist1[w] > 2)
+
 print('Here are the topis we pulled out:')
 for word in words:
 	print('-' + word)
-	
-#Loops through the words in the text and pulls out the nouns
+
+print('\n')
 print('Nouns pulled:')
 is_noun = lambda pos: pos[:2] == 'NN'
-nouns = [word for (word, pos) in nltk.pos_tag(text) if is_noun(pos)]
+nouns = [word for (word, pos) in nltk.pos_tag(clean_tokens) if is_noun(pos)]
 print(nouns)
 
-#Loops through the words in the text and pulls out the collocations (word pairs that go together)
+print('\n')
+print('Collocations:')
+
 colloc = []
-for word in text:
+for word in clean_tokens:
 	if len(word) > 5 and fdist1[word] > 2:
 		colloc.append(word)
 
