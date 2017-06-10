00a8b5841074894860b2a09798e35432d3654f79
Tyler Scionti <tscionti@blogtrackr.com>
2017-06-10 17:13:45 -0400

    Using Text Blob for noun phrases
    Using text blob for noun phrases instead of collocations

--- blogtopic.py
+++ blogtopic.py
@@ -1,4 +1,5 @@
 import nltk
+from textblob import TextBlob, Word
 from nltk.corpus import stopwords
 from nltk import FreqDist, word_tokenize
 from nltk.collocations import *
@@ -6,17 +7,15 @@ import feedparser
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-bigram_measures = nltk.collocations.BigramAssocMeasures()
-
 url = input("Enter a RSS feed url: ")
-
 feed = feedparser.parse(url)
-
 for entry in feed['entries']:
 	content = urlopen(entry['link']).read()
 	soup = BeautifulSoup(content, "html.parser")
 	content = soup.findAll('p')
 	stuff = str(content)
+
+blob = TextBlob(stuff)
 
 text = []
 
@@ -33,7 +32,7 @@ words = sorted(w for w in set(clean_tokens) if len(w) > 2 and fdist1[w] > 2)
 
 print('Here are the topis we pulled out:')
 for word in words:
-	print('-' + word)
+	print('-' + word, fdist1[word] )
 
 print('\n')
 print('Nouns pulled:')
@@ -42,15 +41,5 @@ nouns = [word for (word, pos) in nltk.pos_tag(clean_tokens) if is_noun(pos)]
 print(nouns)
 
 print('\n')
-print('Collocations:')
-
-colloc = []
-for word in clean_tokens:
-	if len(word) > 5 and fdist1[word] > 2:
-		colloc.append(word)
-
-finder = BigramCollocationFinder.from_words(colloc)
-scored = finder.score_ngrams(bigram_measures.raw_freq)
-print(sorted(bigram for bigram, score in scored))
-
-
+print('Nouns phrases:')
+print(blob.noun_phrases)
