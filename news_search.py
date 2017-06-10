f22f839c505a3f5ab54a882efb5d6670514b11f5
Tyler Scionti <tscionti@blogtrackr.com>
2017-06-10 19:45:59 -0400

    Added in standin code

--- news_search.py
+++ news_search.py
@@ -1,1 +1,16 @@
+ import webhoseio
 
+    webhoseio.config(token="97029546-2c7f-4116-a16d-e88dd66f09c2")
+    output = webhoseio.query("filterWebContent", {"q": search-terms " language:english"})
+    print output['posts'][0]['text'] # Print the text of the first post
+    print output['posts'][0]['published'] # Print the text of the first post publication date
+
+    
+# Get the next batch of posts
+
+    output = webhoseio.get_next()
+
+    
+# Print the site of the first post
+
+    print output['posts'][0]['thread']['site']
