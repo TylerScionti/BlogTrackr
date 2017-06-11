ef1e792787f3259b4557871bf721b96df7aba02c
Tyler Scionti <tscionti@blogtrackr.com>
2017-06-10 21:16:12 -0400

    Modified search to just return title and url

--- news_search.py
+++ news_search.py
@@ -1,16 +1,15 @@
- import webhoseio
+import webhoseio
 
-    webhoseio.config(token="97029546-2c7f-4116-a16d-e88dd66f09c2")
-    output = webhoseio.query("filterWebContent", {"q": search-terms " language:english"})
-    print output['posts'][0]['text'] # Print the text of the first post
-    print output['posts'][0]['published'] # Print the text of the first post publication date
+webhoseio.config(token="97029546-2c7f-4116-a16d-e88dd66f09c2")
+output = webhoseio.query("filterWebContent", {"q": "donald trump" " language:english"})
+print(output['posts'][0]['title']) # Print the text of the first post
+print(output['posts'][0]['url']) # Print the text of the first post publication date
 
     
 # Get the next batch of posts
 
-    output = webhoseio.get_next()
+output = webhoseio.get_next()
 
     
 # Print the site of the first post
 
-    print output['posts'][0]['thread']['site']
