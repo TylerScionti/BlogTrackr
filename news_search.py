74a44ceccbe29cceae3cf8807669ad70eb9e37d7
Tyler Scionti <tscionti@blogtrackr.com>
2017-06-11 08:28:46 -0400

    Updated news search to test for multiple keywords

--- news_search.py
+++ news_search.py
@@ -3,6 +3,11 @@ import webhose
 def news_search(keyword):
 	webhose.config(token="97029546-2c7f-4116-a16d-e88dd66f09c2")
 	r = webhose.search(keyword)
-	print(r.total)
 	for i in range(10):
 		print(r.posts[i].title)
+
+#sample keyword output from topic analysis
+keywords = ["Red Sox", "David Ortiz"]
+
+for keyword in keywords:
+	news_search(keyword)
