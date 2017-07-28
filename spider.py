f953df332189c1c185e5a128a287cb8675e94a45
Tyler Scionti <tscionti@blogtrackr.com>
2017-07-27 21:33:49 -0400

    Updated spider to crawl entire site
    Updated spider to crawl entire site, this accepts a domain and starting point then recursively scrapes an entire site following all links on that domain

--- spider.py
+++ spider.py
@@ -1,16 +1,28 @@
 import scrapy
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import HtmlXPathSelector
-from blogtrackr_sample.items import BlogTrackrSampleItem
+from blogtrackr_scraper.items import BlogtrackrScraperItem
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "blogtrackr"
-    start_urls = [
-        'http://blogtrackr.com/blog',
-    ]
+"""
+Uses this class from 'blogtrackr_scraper.items
 
-    def parse(self, response):
-        for title in response.css('h1'):
-        	yield {
-        	'text': title.css('h1').extract()
-        	}
+
+class BlogtrackrScraperItem(scrapy.Item):
+	url = Field() 
+
+"""
+class BlogTrackr(CrawlSpider):
+	name = 'blogtrackr'
+	allowed_domains = ['blogtrackr.com']
+	start_urls = ['http://blogtrackr.com']
+
+	rules = [
+	Rule(LinkExtractor(allow=[r'.*']), callback='parse_item', follow=True)
+	]
+		
+	def parse_item(self, response):
+		href = BlogtrackrScraperItem()
+		href['url'] = response.url
+		return href
