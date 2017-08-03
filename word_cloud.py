93035bfd68f2574a45eff0ec359877ee095627d7
Tyler Scionti <tscionti@blogtrackr.com>
2017-08-02 21:30:59 -0400

    Updated the word cloud
    Edited it to account for the entire blog (pulling from a file containing each URL) as well as removing stopwords from the results.

--- word_cloud.py
+++ word_cloud.py
@@ -1,15 +1,34 @@
 from os import path
+import nltk
+import ssl
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import matplotlib.colors
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = input("Enter a page URL:")
-content = urlopen(url).read()
-soup = BeautifulSoup(content, "html.parser")
-content = soup.findAll('p')
-stuff = str(content)
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+posts = open("out.csv","r").read()
+lines = posts.split('\n')
+
+stuff = ""
+
+for post in lines:
+    try:
+        content = urlopen(post, context=ctx).read()
+        soup = BeautifulSoup(content, "html.parser")
+        content = soup.findAll('p')
+        stop_sym = ['/p', '>' ,'<' ,'p', '/a', 'img', 'alt=', '[', 'span', 'p1', 'href', 'Mailchimp', 'wpcf7 form', 'src', 'https', 'http', 'readmore']
+        filtered = [ x for x in content if x not in stop_sym]
+        stuff += str(filtered)
+    except ValueError:
+        pass
+
+def on_error(self, status):
+	print(status)
 
 colors=['#2F5EB6','#5E8DD9','#F4866A']
 
